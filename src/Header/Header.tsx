ce1f93fe1b73e3361bef90f72941dab2c52ced9e
Robert Prib <robert.prib@gmail.com>
2021-06-05 17:05:52 +1000

    styled header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,20 +1,48 @@
 import React, { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Wrapper = styled.header`
-    background: green;
+    display: flex;
+    flex-direction: column;
+    padding: 4rem 2rem;
+`;
+
+const Title = styled.h1`
+    font-size: 3.5rem;
+    font-family: 'Courier New', Courier, monospace;
+    margin-bottom: 4rem;
+`;
+
+const TitleName = styled.strong`
+    font-family: 'Courier New', Courier, monospace;
+    color: #000;
+    font-weight: 700;
+`;
+
+const Navigation = styled.nav`
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+`;
+
+const NavLink = styled(Link)`
+    text-decoration: none;
+    text-transform: uppercase;
+    color: #68788d;
 `;
 
 export const Header: FunctionComponent = () => {
     return (
         <Wrapper>
-            <h1>() =&gt; &quot;rOBERT pRIB&quot;</h1>
-            <nav>
-                <a>Home</a>
-                <a>About me</a>
-                <a>Contact</a>
-                <a>Test</a>
-            </nav>
+            <Title>
+                () =&gt; &quot;<TitleName>Robert Prib</TitleName>&quot;
+            </Title>
+            <Navigation>
+                <NavLink to="/blog">Home</NavLink>
+                <NavLink to="/blog/about-me">About me</NavLink>
+                <NavLink to="/blog/contact">Contact</NavLink>
+            </Navigation>
         </Wrapper>
     );
 };
